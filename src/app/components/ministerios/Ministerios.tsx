5b60f7058c6990fcc8cc9b4541198e2c41bcd0a6
Agus9217 <justinibarrola92@gmail.com>
2024-09-09 05:43:27 -0300

    cambios actualizados

--- src/app/components/ministerios/Ministerios.tsx
+++ src/app/components/ministerios/Ministerios.tsx
@@ -1,22 +1,80 @@
-import { Flex, Stack } from "@chakra-ui/react"
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Flex,
+  Img,
+  Stack,
+} from "@chakra-ui/react";
 
 export const Ministerios = () => {
   return (
-    <Stack
-    spacing={0}
-    direction={'column'}
-    h={'100dvh'}
-    >
+    <Stack spacing={0} direction={"column"} h={"100dvh"}>
       <Flex
-        borderWidth={'thin'}
-        borderColor={'red'}
+        borderWidth={"thin"}
+        borderColor={"red"}
         flexGrow={1}
-      ></Flex>
-      <Flex
-        borderWidth={'thin'}
-        borderColor={'blue'}
-        flexGrow={1}
-      ></Flex>
+        alignItems={"center"}
+        justifyContent={"center"}
+        gap={8}
+      >
+        <Flex
+          maxW={600}
+          justifyContent={"center"}
+          alignItems={"center"}
+          flexGrow={1}
+          borderWidth={"thin"}
+          borderColor={"red"}
+        >
+          <Img src={"./iglesia-3.jpg"} />
+        </Flex>
+        <Flex
+          flexGrow={1}
+          maxW={500}
+          alignItems={"center"}
+          borderWidth={"thin"}
+          borderColor={"blue"}
+        >
+          <Accordion allowToggle w={'100%'} colorScheme={'teal'}>
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" textAlign="left">
+                    Section 1 title
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" textAlign="left">
+                    Section 2 title
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </Flex>
+      </Flex>
     </Stack>
-  )
-}
+  );
+};
